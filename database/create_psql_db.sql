219c09ec1d2fcb291d276c2aa89372cca7fc6bbc
dgodoy-dev <d.godoy.developer@gmail.com>
2026-04-02 20:16:55 +0200

    fix: update database initialization script for correct role permissions

--- database/create_psql_db.sql
+++ database/create_psql_db.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE IF NOT EXISTS argos_system;
+CREATE DATABASE argos_system;
 
 \c argos_system
 
@@ -8,8 +8,8 @@ BEGIN
         CREATE USER argos WITH PASSWORD 'argos';
     END IF;
 
-    IF NOT EXISTS (SELECT FROM pg_roles rolname = 'sql_agent') THEN
-        CREATE ROLE sql_agent;
+    IF NOT EXISTS (SELECT FROM pg_roles WHERE rolname = 'sql_agent') THEN
+        CREATE ROLE sql_agent WITH LOGIN PASSWORD 'sql_agent';
     END IF;
 END
 $$;
@@ -26,6 +26,7 @@ ALTER USER sql_agent SET search_path TO app;
 GRANT USAGE ON SCHEMA app TO sql_agent;
 GRANT CONNECT ON DATABASE argos_system TO sql_agent;
 GRANT SELECT ON ALL TABLES IN SCHEMA app TO sql_agent;
-ALTER DEFAULT PRIVILEGES IN SCHEMA app GRANT SELECT ON TABLES TO sql_agent;
+ALTER DEFAULT PRIVILEGES FOR ROLE argos IN SCHEMA app GRANT SELECT ON TABLES TO sql_agent;
+ALTER DEFAULT PRIVILEGES FOR ROLE postgres IN SCHEMA app GRANT SELECT ON TABLES TO sql_agent;
 
 CREATE EXTENSION IF NOT EXISTS vector SCHEMA app;
